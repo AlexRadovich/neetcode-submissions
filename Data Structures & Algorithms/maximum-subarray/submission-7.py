cf9a05e3ca39523feccaa4a58016617bb2b11cdc
Alex Radovich <144372177+AlexRadovich@users.noreply.github.com>
2026-05-25 19:46:31 -0600

    Add: maximum-subarray - submission-7

---
+++ Data Structures & Algorithms/maximum-subarray/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        
+        best = current = float('-inf')
+
+        for i in nums:
+            
+
+            if current + i < i:
+                current = i
+            else:
+                current += i
+
+            best = max(best,current)
+
+            
+
+
+        return max(current,best)
+
+        
