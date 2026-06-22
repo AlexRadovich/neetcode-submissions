95ff31612b54a9752a9d10cf385e9c1c76ce778c
Alex Radovich <144372177+AlexRadovich@users.noreply.github.com>
2026-06-22 19:47:30 -0400

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        ix = 0
+        ct = 0
+
+        while ix < len(nums)-1:
+            best = -1
+            bix = ix
+            decay = 0
+            for i in range(ix+1,ix+nums[ix]+1):
+                if i >= len(nums)-1:
+                    return ct +1
+                if decay + nums[i] >= best:
+                    best = decay + nums[i]
+                    bix = i
+                decay += 1
+            ix = bix
+            ct += 1
+
+        return ct
